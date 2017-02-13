d7ee338a005f6da6b21405dd61196d6d894afe88
NabilB <yoyohko@gmail.com>
2017-02-13 19:57:40 +0100

    Update Reservation.php

--- application/models/Reservation.php
+++ application/models/Reservation.php
@@ -13,4 +13,13 @@ class Reservation extends CI_Model
 	{
 		
 	}
+	
+	public function updateCalendar () {
+		$bdd = new PDO("pgsql:host=localhost;dbname=reservation;user=walidb;password=CZj86b4k");
+
+		foreach($bdd->query("SELECT reservation.nom,to_char(debut, 'YYYY-MM-DD') || 'T' || to_char(debut, 'HH24:MI:SS') AS 					debut,to_char(fin, 'YYYY-MM-DD') || 'T' || to_char(fin, 'HH24:MI:SS') AS fin FROM reservation") as $row) {
+			$donnee = $row["nom"] . "   ////   " . $row["debut"] . "   ////   " . $row["fin"] . "<br />";
+			echo $donnee;
+		}
+	}
 }
